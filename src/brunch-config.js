bdb0a40fc37221704a7852465da66efffb1cb988
seble <me@seble.info>
2017-09-12 21:15:49 +0200

    Initial commit

---
+++ src/brunch-config.js
@@ -0,0 +1,14 @@
+// See http://brunch.io for documentation.
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: 'degschta.js'
+    },
+    stylesheets: {
+      joinTo: 'degschta.css'
+    },
+  },
+  paths: {
+    public: '../'
+  }
+};

09f693c5079532fa0e07ed81fac32bf728a27095
seble <me@seble.info>
2017-09-12 22:52:04 +0200

    tawk.to and bigger font size

--- src/app/initialize.js
+++ src/app/initialize.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // do your setup here
-  console.log('Initialized app');
+  tawkto();
 });
+
+function tawkto() {
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/59b84114c28eca75e461fab7/default';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+}
